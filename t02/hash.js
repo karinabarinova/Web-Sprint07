7706fc51ad16e8ba849ee923e6c3a5c73c233d1d
Karina Barinova <43203585+karinabarinova@users.noreply.github.com>
2020-12-24 13:21:39 +0200

    finished task

---
+++ t02/hash.js
@@ -0,0 +1,13 @@
+'use strict';
+var crypto = require('crypto');
+
+function hash(password, salt){
+    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    var value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+}
+module.exports = hash;
